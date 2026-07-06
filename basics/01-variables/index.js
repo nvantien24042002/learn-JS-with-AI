ebb637faac6ea727fa73fd157eb39db5c5f6214f
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-06 17:07:35 +0700

    learn: variable and operator

---
+++ basics/01-variables/index.js
@@ -0,0 +1,5 @@
+const myName = "Tên Của Bạn";
+let yearLearn = 1;
+
+console.log("Tên của mình là:", myName);
+console.log("Năm học tập:", yearLearn);
---
+++ basics/02-operator/index.js
@@ -0,0 +1,9 @@
+let score = 10;
+let bonus = 5;
+
+// Use arithmetic operators
+let totalScore = score + bonus;
+let remainder = 10 % 3; // What will this be?
+
+console.log("Total Score:", totalScore);
+console.log("Remainder of 10/3:", remainder);
