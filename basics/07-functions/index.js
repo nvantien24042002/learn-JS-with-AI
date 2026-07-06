73ff46c4f53657d07d84dfd25c1afb825981f01b
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-07 05:04:14 +0700

    feat: complete lesson 07 - functions

---
+++ basics/07-functions/index.js
@@ -0,0 +1,12 @@
+function sayHello(name) {
+    return "Hello, " + name + "!";
+}
+
+// Gọi hàm
+let message = sayHello("Tien");
+alert(message); // Output: "Hello, Tien!"
+
+function calculateArea(width,height){
+    return width * height;
+}
+console.log(calculateArea(5,10))
