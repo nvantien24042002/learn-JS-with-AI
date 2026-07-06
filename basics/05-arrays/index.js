c3ba6255289a0cac835f9744fd3dff9e6955f10c
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-06 19:49:51 +0700

    feat: complete lesson 05 - arrays

---
+++ basics/05-arrays/index.js
@@ -0,0 +1,9 @@
+let fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits)
+console.log(fruits[0])
+console.log(fruits[1])
+
+let task = ["Clean room", "Do laundry", "Study JS"];
+task.push("Go to gym")
+console.log(task);
+console.log(task.length);
