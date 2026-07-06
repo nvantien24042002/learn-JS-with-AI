141cb228f81968bb977c368f7fa10ad006542842
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-06 19:37:57 +0700

    feat: complete lesson 04 - loops

---
+++ basics/04-loops/index.js
@@ -0,0 +1,13 @@
+// for (initialization; condition; iteration)
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Count is:", i);
+// }
+//while
+let i = 1;
+while (i <= 5) {
+    console.log("While count:", i);
+    i++; // Don't forget to increase i, otherwise you'll get an infinite loop!
+}
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+}
