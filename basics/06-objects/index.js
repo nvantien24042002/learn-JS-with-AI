b7c3493371bc248029dfc474d8f0736ebb8b1505
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-06 20:12:00 +0700

    feat: complete lesson 06 - objects

---
+++ basics/06-objects/index.js
@@ -0,0 +1,19 @@
+let car = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2026,
+    isElectric: false
+};
+console.log(car.brand);
+console.log(car.year);
+car.year = 2027;           // Update existing value
+car.color = "Red";         // Add a new property
+
+let student  = {
+    name:"Nguyen Van Tien",
+    age:24,
+    grade: "IT"
+};
+console.log(student.name);
+student.grade = 9;
+student.isEnrolled = true;
