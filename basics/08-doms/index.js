7bfc49f52970d4c7488f3443294e2cf38c73ede8
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-07 12:53:18 +0700

    feat: display student list using innerHTML

--- basics/08-doms/index.js
+++ basics/08-doms/index.js
@@ -7,3 +7,14 @@ button.addEventListener("click",function(){
     alert("Button clicked!")
 })
 
+let classroom = [
+    { name: "Nguyễn Văn Tiến", grade: 9 },
+    { name: "Pham Van A", grade: 10 },
+    { name: "Ly Van C", grade: 10 }
+]
+const listElement = document.getElementById("studentList");
+let htmlContent = ""; // Tạo một biến chứa chuỗi HTML
+for(let i = 0;i < classroom.length;i++){
+    htmlContent += "<li>"+ classroom[i].name+ " - Điểm: "+ classroom[i].grade + "</li>";
+}
+listElement.innerHTML = htmlContent;
