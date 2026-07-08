a961dcfde7f4d048ceb1cd1f4b1d5a30c4811ebc
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-08 13:01:25 +0700

    feat: complete CRUD operations for student list

--- basics/projects/To-do-list/index.js
+++ basics/projects/To-do-list/index.js
@@ -7,7 +7,9 @@ function renderStudent(){
     console.log("Mảng hiện tại của tôi là:", listStudentData);
     let htmlContent = "";
     for(let i = 0; i < listStudentData.length; i++){
-        htmlContent+="<li>" + listStudentData[i] + "</li>";
+        const stduent = listStudentData[i];
+        console.log(stduent);
+        htmlContent +=`<li>${stduent.name} <button onclick=deleteStudent(${stduent.id})>Xóa</button></li>`;
     }
     listStudent.innerHTML = htmlContent;
 }
@@ -22,9 +24,20 @@ function addStudentNew(event){
         alert("Bạn chưa nhập tên học sinh!");
         return; // Dừng hàm tại đây nếu không có dữ liệu
     }
-    listStudentData.push(studentName);
+    // Tạo đối tượng sinh viên với object duy nhất
+    const newStudent = {
+        id: Date.now(),
+        name:studentName,
+    };
+    listStudentData.push(newStudent);
     // 5. Làm sạch ô input
     nameStudent.value = "";
     renderStudent();
 }
 addStudentBtn.addEventListener("click",addStudentNew)
+function deleteStudent(id){
+    listStudentData = listStudentData.filter(function(student){
+        return student.id !== id;
+    })
+    renderStudent();
+}
