2ef16cc0512d9e651a945220ef12e6105c0481ca
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-09 07:45:44 +0700

    feat: complete map() basic exercise - discount price calculation

--- advance/02- Map/index.js
+++ advance/02- Map/index.js
@@ -3,6 +3,15 @@ const numbers = [1,2,3];
 const double = numbers.map(n => n * 2);
 console.log(double);
 
-const prices = [10, 20, 30];
-const addChar = prices.map(p => `$${p}`);
-console.log(addChar);
+// const prices = [10, 20, 30];
+// const addChar = prices.map(p => `$${p}`);
+// console.log(addChar);
+/**
+ * Bài 1: Tính toán trên mảng (Cơ bản)
+ * Đề bài: Bạn có mảng giá gốc của các sản phẩm: const prices = [100, 200, 300];.
+ *          Hãy tạo ra một mảng mới là giá sau khi đã giảm 10% (tức là nhân với 0.9).
+ */
+const prices = [100, 200, 300];
+
+const discountPrice = prices.map(price =>(price*0.9));
+console.log(discountPrice);
